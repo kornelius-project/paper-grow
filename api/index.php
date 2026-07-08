c6a8b7be6aab1aff8f8ebfba8655d03a02d0aaf7
Kornelius Candra <krnlscdra9@gmail.com>
2026-07-08 23:21:47 +0700

    Mengaktifkan display_errors untuk debug Vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Teruskan request dari Vercel Serverless ke file public/index.php bawaan Laravel
 require __DIR__ . '/../public/index.php';
